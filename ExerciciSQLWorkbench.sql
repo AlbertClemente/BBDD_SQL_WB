8cc2bb89189d5823628d6cc30938cceb7439caaa
M022683\formacio <>
2018-07-10 11:12:15 +0200

    Afegeixo exercici 2

--- ExerciciSQLWorkbench.sql
+++ ExerciciSQLWorkbench.sql
@@ -1,2 +1,6 @@
 -- 1.Quantitat de registres de la taula de vols:
 SELECT count(flightID) FROM usairlineflights.flights;
+
+-- 2. Retard promig de sortida i arribada segons l’aeroport origen.
+SELECT avg(ArrDelay) FROM usairlineflights.flights;
+SELECT avg(DepDelay) FROM usairlineflights.flights;
