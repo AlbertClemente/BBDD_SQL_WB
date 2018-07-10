193704ffdf6a3c8449a0193b1d309ed5ba4f6db1
M022683\formacio <>
2018-07-10 12:10:53 +0200

    Añadido el ejercicio 3

--- ExerciciSQLWorkbench.sql
+++ ExerciciSQLWorkbench.sql
@@ -3,3 +3,17 @@ SELECT count(flightID) FROM usairlineflights.flights;
 
 -- 2. Retard promig de sortida i arribada segons l’aeroport origen.
 SELECT origin as 'Aeroport Origen', avg(ArrDelay) as 'Retard Arribada', avg(DepDelay) as 'Retard Sortida' FROM usairlineflights.flights group by origin;
+
+/* 3. Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen. A més,
+	volen que els resultat es mostrin de la següent forma:
+	LAX, 2000, 01, retard
+	LAX, 2000, 02, retard
+	…
+	LAX, 2000, 12, retard
+	LAX, 2001, 01, retard
+	…
+	ONT, 2000, 01, retard
+	ONT, 2000, 02, retard
+	etc.*/
+    
+SELECT origin as 'Aeroport Origen', colYear as 'Any', colMonth as 'Mes',  avg(ArrDelay) as 'Retard Arribada' FROM usairlineflights.flights group by origin;
